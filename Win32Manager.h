a484e64211e2c8035199e547006c5e55bb428b69
g--o <goproducti@gmail.com>
2013-08-23 18:27:09 +0300

    Create Win32Manager.h

---
+++ Win32Manager.h
@@ -0,0 +1,14 @@
+//Loading required stuff
+#pragma comment( lib, "Winmm.lib" )
+#include <windows.h>
+
+	//Defining stuff
+	int screen_height; 
+	int screen_width;
+	HWND wndHandle;
+
+
+// The Win32 C++ Manager
+// Might create one later...
+
+
